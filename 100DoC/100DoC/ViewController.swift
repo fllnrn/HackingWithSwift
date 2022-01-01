eeb4e5495079d151f09b2db4842986c08168c750
fllnrn <lazy@home.local>
2022-01-01 17:57:04 +0300

    init

---
+++ 100DoC/100DoC/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  100DoC
+//
+//  Created by Андрей Гавриков on 01.01.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+        
+    }
+
+
+}
+
