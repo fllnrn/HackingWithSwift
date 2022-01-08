9031df6bac3e2f5475e82c3449a1ee30a53c3695
fllnrn <lazy@home.local>
2022-01-08 20:54:48 +0300

    Project10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Андрей Гавриков on 08.01.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
