d54b680bd4a829d8463568311db8fcc90ad1da15
fllnrn <lazy@home.local>
2022-01-12 16:01:32 +0300

    swift lint

--- Project16/Project16/Capital.swift
+++ Project16/Project16/Capital.swift
@@ -12,7 +12,7 @@ class Capital: NSObject, MKAnnotation {
     var title: String?
     var coordinate: CLLocationCoordinate2D
     var info: String
-    
+
     init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
         self.info = info
         self.title = title
